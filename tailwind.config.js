423c797fd635b46b6df2a74d11f416c60fae2fd7
max00317 <antoni.fullstackdev@gmail.com>
2023-05-12 21:27:39 +0900

    fix grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
+const withMT = require('@material-tailwind/react/utils/withMT')
 module.exports = withMT({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages//*.{html,js}',
+    './src/components//.{html,js}',
+    './build/server/pages/**/.{html,js}',
   ],
   theme: {
     extend: {
